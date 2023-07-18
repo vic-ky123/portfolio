d76c945755cd5230583d0ba402d5b5400cab60ed
vic-ky123 <vigneshlokeshbabu131@gmail.com>
2023-07-19 00:36:43 +0530

    Minor Update

---
+++ src/components/FormComponents/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <>
+            <div>
+                <p>Input</p>
+            </div>
+        </>
+    )
+};
+
+export default Input;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <p>Navbar</p>
+            </div>
+        </>
+    )
+};
+
+export default Navbar;
